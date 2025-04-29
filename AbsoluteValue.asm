2f54aa72e9d6cda3658e58a571a481b1718f404a
Adam-o6 <vxff0627@leeds.ac.uk>
2025-04-29 21:23:20 +0000

    w

--- AbsoluteValue.asm
+++ AbsoluteValue.asm
@@ -1,82 +1,65 @@
-// Load R0 into D
+// Copy R0 to R1
 @R0
 D = M
+@R1
+M = D
 
-// If D >= 0, jump to POSITIVE
-@POSITIVE
-D; JGE
+// Check if x is negative using sign bit
+// Load R0 and AND with 0x8000
+@R0
+D = M
+@SIGN_CHECK
+@32768      // 0x8000
+D = D & A   // D = R0 & 0x8000
+@NEGATIVE
+D; JNE
 
-// Set R2 = 1
+// positive
 @R2
-M = 1
+M = 0       // Set R2 = 0
+@R3
+M = 0       // Set R3 = 0
+@END
+0; JMP
 
-// Reload R0 into D
+// negative
+(NEGATIVE)
+@R2
+M = 1       // Set R2 = 1
+
+// Check if x == -32768
 @R0
 D = M
-
-// Try detecting MIN_VALUE by adding 2
-@MIN_VALUE
-D = D + 1
-@MIN_VALUE
-D = D + 1
-
-// If D == 0, can't compute (MIN_VALUE)
-@CANNOT_COMPUTE
+@CHECK_MIN
+@MINVALUE
+D = D - A   // D = R0 - MINVALUE
+@IS_MIN
 D; JEQ
 
-// Otherwise, negate R0
+// negative and not -32768
 @R0
 D = M
 D = -D
-
-// Store result in R1
 @R1
-M = D
-
-// Set R3 = 0
+M = D       // Store absolute value in R1
 @R3
 M = 0
-
-// End
 @END
 0; JMP
 
-(POSITIVE)
-
-// Set R2 = 0
-@R2
-M = 0
-
-// Copy R0 to R1
-@R0
-D = M
-@R1
-M = D
-
-// Set R3 = 0
-@R3
-M = 0
-
-// End
-@END
-0; JMP
-
-(CANNOT_COMPUTE)
-
-// Set R3 = 1
+// MIN VALUE
+(IS_MIN)
 @R3
 M = 1
 
-// Copy R0 to R1
-@R0
-D = M
-@R1
-M = D
-
-// End
 @END
 0; JMP
 
+// END
 (END)
 @END
+0; JMP
 
+// CONSTANTS
+(MINVALUE)   // -32768 = 0x8000
+@32768
