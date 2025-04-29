8b5a4a5170cc3d95dd51355cdccb776da6123583
Adam-o6 <vxff0627@leeds.ac.uk>
2025-04-29 20:52:32 +0000

    w

--- AbsoluteValue.asm
+++ AbsoluteValue.asm
@@ -1,4 +1,4 @@
-// Load input value from R0 into D
+// Load R0 into D
 @R0
 D = M
 
@@ -6,25 +6,25 @@ D = M
 @POSITIVE
 D; JGE
 
-// If D < 0, we might be dealing with the minimum possible value
+// Set R2 = 1
 @R2
 M = 1
 
-// Reload D with the value from R0
+// Reload R0 into D
 @R0
 D = M
 
-// Increment D twice to detect overflow
+// Try detecting MIN_VALUE by adding 2
 @MIN_VALUE
 D = D + 1
 @MIN_VALUE
 D = D + 1
 
-// If it overflows
+// If D == 0, can't compute (MIN_VALUE)
 @CANNOT_COMPUTE
 D; JEQ
 
-// Negate the value since it's not the minimum possible
+// Otherwise, negate R0
 @R0
 D = M
 D = -D
@@ -33,44 +33,50 @@ D = -D
 @R1
 M = D
 
-// Set R3 to 0
+// Set R3 = 0
 @R3
 M = 0
 
-// End of program
+// End
 @END
 0; JMP
 
-// Positive Input
 (POSITIVE)
+
+// Set R2 = 0
 @R2
 M = 0
 
+// Copy R0 to R1
 @R0
 D = M
-
 @R1
 M = D
 
+// Set R3 = 0
 @R3
 M = 0
 
+// End
 @END
 0; JMP
 
-// Minimum Value
 (CANNOT_COMPUTE)
+
+// Set R3 = 1
 @R3
 M = 1
 
+// Copy R0 to R1
 @R0
 D = M
-
 @R1
 M = D
 
+// End
 @END
 0; JMP
 
 (END)
 @END
+
