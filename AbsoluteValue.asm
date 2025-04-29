d4b4e9697d18a54e7c9c47ee791410e7c2edf847
Adam-o6 <vxff0627@leeds.ac.uk>
2025-04-29 20:35:01 +0000

    w

--- AbsoluteValue.asm
+++ AbsoluteValue.asm
@@ -1,49 +1,76 @@
+// Load input value from R0 into D
 @R0
-    D = M         
-    @POSITIVE
-    D; JGE        
-    @R2
-    M = 1        
-    @R0
-    D = M       
-    @MIN_VALUE
-    D = D + 1   
-    @MIN_VALUE
-    D = D + 1   
-    @CANNOT_COMPUTE
-    D; JEQ        
+D = M
 
-    @R0
-    D = M         
-    D = -D         
-    @R1
-    M = D         
-    @R3
-    M = 0          
-    @END
-    0; JMP         
+// If D >= 0, jump to POSITIVE
+@POSITIVE
+D; JGE
 
+// If D < 0, we might be dealing with the minimum possible value
+@R2
+M = 1
+
+// Reload D with the value from R0
+@R0
+D = M
+
+// Increment D twice to detect overflow
+@MIN_VALUE
+D = D + 1
+@MIN_VALUE
+D = D + 1
+
+// If it overflows
+@CANNOT_COMPUTE
+D; JEQ
+
+// Negate the value since it's not the minimum possible
+@R0
+D = M
+D = -D
+
+// Store result in R1
+@R1
+M = D
+
+// Set R3 to 0
+@R3
+M = 0
+
+// End of program
+@END
+0; JMP
+
+// Positive Input
 (POSITIVE)
-    @R2
-    M = 0         
-    @R0
-    D = M         
-    @R1
-    M = D          
-    @R3
-    M = 0         
-    @END
-    0; JMP        
+@R2
+M = 0
 
+@R0
+D = M
+
+@R1
+M = D
+
+@R3
+M = 0
+
+@END
+0; JMP
+
+// Minimum Value
 (CANNOT_COMPUTE)
-    @R3
-    M = 1          
-    @R0
-    D = M         
-    @R1
-    M = D          
-    @END
-    0; JMP         
+@R3
+M = 1        // Status = error
+
+@R0
+D = M
+
+@R1
+M = D        // Store original value in R1 anyway
+
+@END
+0; JMP
 
 (END)
-    @END
+@END
--- Xor.asm
+++ Xor.asm
@@ -1,22 +1,47 @@
+// Load R0 into D
 @R0
-D=M         
-@R1
-M=!M        
+D = M
+
+// Store original R0 in R3
 @R3
-M=D&M        
-@R0
-D=!M          
+M = D
+
+// Load R1 into D
 @R1
-M=!M         
+D = M
+
+// Compute NOT R1 into R4
 @R4
-M=D
-@R1
-D=M
-@R4
-M=D&M  
+M = !D
+
+// Load R3 into D
 @R3
-D=M    
-@R4
-D=D|M        
+D = M
+
+// R5 = R0 AND NOT R1
+@R5
+M = D & M
+
+// Compute NOT R0 into D
+@R3
+D = M
+@R6
+M = !D
+
+// Load R1 into D again
+@R1
+D = M
+
+// R7 = NOT R0 AND R1
+@R7
+M = D & M
+
+// XOR
+@R5
+D = M
+@R7
+D = D | M
+
+// Store final result in R2
 @R2
-M=D
+M = D
